91fa9d9823218d27b26b2766ac026d57da20dfd2
rakgogia <rakgogia@umich.edu>
2018-02-19 14:53:59 -0500

    Added test for exponentiation operator, travis build should fail try3

--- test_rpn.py
+++ test_rpn.py
@@ -6,6 +6,7 @@ class TestBasics(unittest.TestCase):
 		result_add = rpn.calculate('1 1 +')
 		self.assertEqual(2, result_add)
 
+
     def test_exp(self):
         result_exp = rpn.calculate('2 3 ^')
         self.assertEqual(8, result_exp)
